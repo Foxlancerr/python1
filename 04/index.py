d0122e666e21aebd4ba1ccbe46355fdf160cb89b
M-Asimm <kpkprince55persia@gmail.com>
2024-01-16 10:42:35 +0500

    numpy basics operation

---
+++ 04/index.py
@@ -0,0 +1,38 @@
+import numpy as np
+# arr1 = np.array([1, 2, 3, 4])
+
+# print(arr1)
+# print(arr1.dtype) # what type of array int64,int32,int16,float,string
+# print(arr1.ndim) # what type of dimention single dementional,double dimentional
+# print(arr1.size) # How many elements present it
+
+# arr1 = np.array([[1, 2, 3, 4],[3,6,23,53]])
+# arr2 = np.array([[3,5,5,2],[1, 2, 3, 4],[3,6,23,53]])
+# print(arr1.shape) # how many rows and how many column present
+# print(arr2.shape) # how many rows and how many column present
+
+# arr3 = np.zeros((2,2))
+# print(arr3.nbytes)
+# arr3 = np.ones((3,3))
+# print(arr3)
+
+# arr5 = np.full((3,3),"r")
+# print(arr5)
+# print(arr5.size)
+
+# arr6 = np.array([[[45,23,100]]])
+# print(arr6.itemsize)
+
+# arr7 = np.identity((5))
+# print(arr7)
+
+# arr8 = np.array([3,-7,-3])
+# print(np.cos(arr8))
+# print(np.sin(arr8))
+# print(np.abs(arr8))
+
+# arr9 = np.arange(0,30,5)
+# print(arr9)
+
+arr10 = np.linspace(1,10,5)
+print(arr10)
